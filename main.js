add19102070c71c8f47fe6ba5ff7907b2f65099a
tina.mart <tina.mart48@gmail.com>
2021-01-11 19:05:03 +0300

    Working on project Block

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.onscroll = scroller;
 let scrolling = 0,
   change = document.querySelector('.page-header')
 
-  function.scroller() {
+  function scroller() {
   if(window.pageYOffset > scrolling){
     scrolling = window.pageYOffset;
     change.className = 'absolute';
